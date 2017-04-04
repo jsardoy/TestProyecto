a9884d386fa530a290aafaa71e7b686af0dd32d1
jsardoy <jmartinsardoy@gmail.com>
2017-04-04 15:00:54 -0300

    Update dep2.php

--- dep2.php
+++ dep2.php
@@ -1,10 +1,9 @@
 <?
 function multiply($n,$x){
-	$res=0;
-	foreach($i=0; $i<$x; $i++){
-		$res+=$n;
-	}
-	return $n;
+	return $n*$x;
+}
+function dividir($n,$x){
+	return $/$x;
 	//IMPROVEMENT: return $n*$x;
 }
 ?>
